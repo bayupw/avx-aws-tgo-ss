65fbb599633fd99dfde61cbb69120717e9a0a8b5
bayupw <bayupw@gmail.com>
2022-03-23 19:26:56 +1300

    update transit

--- transit_peering.tf
+++ transit_peering.tf
@@ -10,7 +10,7 @@
 } */
 
 resource "aviatrix_transit_gateway_peering" "stass_dev_peering" {
-  transit_gateway_name1           = aviatrix_transit_gateway.stass_gw.gw_name
+  transit_gateway_name1           = aviatrix_transit_gateway.ss_gw.gw_name
   transit_gateway_name2           = aviatrix_transit_gateway.dev_gw.gw_name
   gateway1_excluded_network_cidrs = ["0.0.0.0/0"]
   gateway2_excluded_network_cidrs = ["0.0.0.0/0"]
